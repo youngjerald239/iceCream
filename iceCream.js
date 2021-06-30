05d94abc9197d2afde600ff22efa342e1d1b8f61
youngjerald239 <youngjerald239@gmail.com>
2021-06-30 15:10:10 -0500

    first commit

---
+++ iceCream.js
@@ -0,0 +1,15 @@
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+}
+
+let order = (call_production) =>{
+
+    console.log("Order placed. Please call production")
+    //Function that is being called
+    call_production()
+}
+
+let production = () =>{}
