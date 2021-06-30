f523fbe93209a897677bdd430cae4cdc3a761622
youngjerald239 <youngjerald239@gmail.com>
2021-06-30 15:23:08 -0500

    fruit has been chopped

--- iceCream.js
+++ iceCream.js
@@ -5,18 +5,47 @@ let stocks = {
     toppings : ["chocolate", "peanuts"],
 }
 
-let order = (call_production) =>{
+// let order = (call_production) =>{
 
-    console.log("Order placed. Please call production")
-    //Function that is being called
-    call_production()
-}
+//     console.log("Order placed. Please call production")
+//     //Function that is being called
+//     call_production()
+// }
 
-let production = () =>{
+// let production = () =>{
 
-    console.log("Production has started")
-}
+//     console.log("Production has started")
+// }
 
-// Trigger 👇
+// // Trigger 👇
 
-order(production)
+// order(production)
+
+// Function 1
+
+let order = (fruit_name, call_production) =>{
+    setTimeout(function(){
+
+        console.log(`${stocks.Fruits[fruit_name]} was selected`)
+    
+    // Order placed. Call production to start
+       call_production()
+      },2000)
+  }
+  
+  // Function 2
+  
+  let production = () =>{
+    setTimeout(()=>{
+        console.log("production has started")
+        setTimeout(()=>{
+            console.log("The fruit has been chopped")
+          },2000)
+
+      },0000)
+  }
+  
+  
+  // Trigger 👇
+  
+  order(0, production);
